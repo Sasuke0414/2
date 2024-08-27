479a007c7ed11fdb1991a07e97f937abf12d22f0
Abdulloh <163301172+Sasuke0414@users.noreply.github.com>
2024-08-27 15:05:19 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# # name = "Abdulloh"
+# # age = "16"
+# # hasCar = False
+
+multik = input("men eng yomon ko'rgan multfilm:")
+fudbolci = input("eng yaxshi futbol o'ynaydigan futbolchi:")
+narsa = input("Marsda menga yoqadigan eng yaxshi narsa:")
+sayohat = input("kelajakda qaysi davlatlatga sayohat qilishim:")
+moshina = input("kelajakda nechta mashinam bo'lishi:")
+moshina2= input("men eng yaxshi ko'rgan mashina nomi 2:")
+
+print( f"men eng yomon ko'rgan multfilm: {multik} eng yaxshi futbol o'ynaydigan futbolchi: {fudbolci}, Marsda menga yoqadigan eng yaxshi narsa: {narsa}, kelajakda qaysi davlatlatga sayohat qilishim: {sayohat}, kelajakda nechta mashinam bo'lishi: {moshina}, men eng yaxshi ko'rgan mashina nomi 2: {moshina2}:")
+
+# ------------------------------------------------------------------------------------------
+
+# Animal = str(input("Hayvon:"))
+# height = int(input("Ogirligi:"))
+# color = input("Rangi:")
+# age = int(input("Yoshi:"))
+# name = input("Ismi:")
+
+# print(f"Hayvon: {Animal}, Og'irligi: {height}, Rangi: {color}, Yoshi: {age}, Ismi: {name} ")
+
+
